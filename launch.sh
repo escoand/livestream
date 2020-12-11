6ae25779904016e22a98cbe43185eb67701ee691
escoand <escoand@users.noreply.github.com>
2020-12-11 23:23:41 +0100

    revert splash

--- launch.sh
+++ launch.sh
@@ -6,7 +6,7 @@ SPLASH_SOCK=/var/run/fbi.sock
 rm -f "$SPLASH_SOCK"
 mkfifo "$SPLASH_SOCK" &&
 tail -f "$SPLASH_SOCK" |
-fbi -a -d /dev/fb0 -cachemem 10 -readahead -noverbose /usr/local/share/splash/{boot.png,wifi.jpg,nowifi.jpg,stream.jpg} &
+fbi -a -d /dev/fb0 -cachemem 10 -readahead -noverbose /usr/local/share/splash/{wifi.jpg,nowifi.jpg,stream.jpg} &
 sleep 1
 
 # install boot splash
@@ -17,20 +17,20 @@ if [ -b /dev/mmcblk0p1 ]; then
 fi
 
 # check wifi
-printf 1g >> "$SPLASH_SOCK"
+echo > "$SPLASH_SOCK"
 iwgetid -r
 
 # wifi config
+echo > "$SPLASH_SOCK"
 if [ $? -eq 0 ]; then
     echo 'Skipping WiFi Connect'
 else
     echo 'Starting WiFi Connect'
-    printf 2g >> "$SPLASH_SOCK"
     wifi-connect
 fi
 
 # start stream
-printf 3g >> "$SPLASH_SOCK"
+echo > "$SPLASH_SOCK"
 python3 /usr/local/bin/youtube-dl -f mp4 -g "$STREAM_URL" |
 xargs -r omxplayer -o hdmi
 
