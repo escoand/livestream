9a2f38e700ab4de797b9e7ce0b3c36cfdc848c4d
escoand <escoand@users.noreply.github.com>
2020-12-11 15:54:05 +0100

    use correct splash images

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # check wifi
-fbi -a -d /dev/fb0 /usr/local/share/splash/splash-wifi.jpg </dev/null
+fbi -a -d /dev/fb0 /usr/local/share/splash/splash-wifi.png </dev/null
 iwgetid -r
 
 # wifi config
@@ -9,11 +9,11 @@ if [ $? -eq 0 ]; then
     printf 'Skipping WiFi Connect\n'
 else
     printf 'Starting WiFi Connect\n'
-    fbi -a -d /dev/fb0 /usr/local/share/splash/splash-nowifi.jpg </dev/null
+    fbi -a -d /dev/fb0 /usr/local/share/splash/splash-nowifi.png </dev/null
     wifi-connect
 fi
 
 # start stream
-fbi -a -d /dev/fb0 /usr/local/share/splash/splash-stream.jpg </dev/null
+fbi -a -d /dev/fb0 /usr/local/share/splash/splash-stream.png </dev/null
 python3 /usr/local/bin/youtube-dl -f mp4 -g "$STREAM_URL" |
 xargs -tr omxplayer -o hdmi
