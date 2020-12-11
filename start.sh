adbe52b72aec960f17cf7a393253bec435436e18
escoand <escoand@users.noreply.github.com>
2020-12-11 22:51:10 +0100

    wait for fbi

--- start.sh
+++ start.sh
@@ -6,7 +6,8 @@ SPLASH_SOCK=/var/run/fbi.sock
 rm -f "$SPLASH_SOCK"
 mkfifo "$SPLASH_SOCK" &&
 tail -f "$SPLASH_SOCK" |
-fbi -a -d /dev/fb0 -cachemem 10 -readahead -noverbose /usr/local/share/splash/{wifi.jpg,nowifi.jpg,stream.jpg} &
+fbi -a -d /dev/fb0 -cachemem 10 -readahead -noverbose /usr/local/share/splash/{boot.png,wifi.jpg,nowifi.jpg,stream.jpg} &
+sleep 1
 
 # install boot splash
 if [ -b /dev/mmcblk0p1 ]; then
@@ -16,7 +17,7 @@ if [ -b /dev/mmcblk0p1 ]; then
 fi
 
 # check wifi
-echo > "$SPLASH_SOCK"
+echo >> "$SPLASH_SOCK"
 iwgetid -r
 
 # wifi config
@@ -24,12 +25,12 @@ if [ $? -eq 0 ]; then
     echo 'Skipping WiFi Connect'
 else
     echo 'Starting WiFi Connect'
-    echo > "$SPLASH_SOCK"
+    echo >> "$SPLASH_SOCK"
     wifi-connect
 fi
 
 # start stream
-echo > "$SPLASH_SOCK"
+echo >> "$SPLASH_SOCK"
 python3 /usr/local/bin/youtube-dl -f mp4 -g "$STREAM_URL" |
 xargs -r omxplayer -o hdmi
 
