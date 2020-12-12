f387b91ff7d9664a6f18e405ee78dfc06c363854
escoand <escoand@users.noreply.github.com>
2020-12-12 18:54:20 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -e
+
+# requirements
+grep -q '^http.*/community$' ||
+sed -n 's|^\(http.*\)/main$|\1/community|p' /etc/apk/repositories >>/etc/apk/repositories
+apk add --no-cache curl dnsmasq fbida-fbi hostapd omxplayer py3-pip python3
+pip install youtube-dl
+
+# livestream
+curl -Ls -o /usr/local/bin/livestream https://raw.githubusercontent.com/escoand/livestream/master/launch.sh
+cat <<'END' >/etc/init.d/livestream
+#!/sbin/openrc-run
+command=/usr/local/bin/livestream
+command_background=true
+END
+chmod +x /etc/init.d/livestream /usr/local/bin/livestream
+rc-update add livestream default
+
+# splash
+curl -Ls https://raw.githubusercontent.com/escoand/livestream/master/splash.sh |
+sh -s
+
+# persist
+lbu add /etc/init.d/livestream /usr/local/bin/livestream /usr/local/share/splash/
+lbu commit -d
