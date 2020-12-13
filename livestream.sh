e690e2ff7ed9637f1244afe72462c9f0d8550e89
escoand <escoand@users.noreply.github.com>
2020-12-13 13:10:02 +0100

    Update livestream.sh

--- livestream.sh
+++ livestream.sh
@@ -4,18 +4,18 @@ SPLASH_SOCK=/run/livestream/fbi.sock
 SPLASH_DIR=/usr/local/share/splash
 
 # wifi config
-fbv "$SPLASH_DIR/check.jpg"
+fbv -cei "$SPLASH_DIR/check.jpg"
 echo >> "$SPLASH_SOCK"
 if iwgetid -r; then
     echo 'Skipping WiFi Connect'
 else
-    fbv "$SPLASH_DIR/wifi.jpg"
+    fbv -cei "$SPLASH_DIR/wifi.jpg"
     echo 'Starting WiFi Connect'
     wifi-connect
 fi
 
 # start stream
-fbv "$SPLASH_DIR/stream.jpg"
+fbv -cei "$SPLASH_DIR/stream.jpg"
 VIDEO=$(youtube-dl -f mp4 -g "$STREAM_URL" 2>&1)
 case "$VIDEO" in
     http*)
