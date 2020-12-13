99ed349164884b2e055038e5db7ef4cae2d6ecf6
escoand <escoand@users.noreply.github.com>
2020-12-13 13:06:07 +0100

    use fbv

--- livestream.sh
+++ livestream.sh
@@ -3,31 +3,19 @@
 SPLASH_SOCK=/run/livestream/fbi.sock
 SPLASH_DIR=/usr/local/share/splash
 
-# splash chain
-rm -f "$SPLASH_SOCK"
-mkdir -p /run/livestream
-mkfifo "$SPLASH_SOCK"
-tail -f "$SPLASH_SOCK" |
-fbi -a -d /dev/fb0 -cachemem 10 -readahead -noverbose \
-    "$SPLASH_DIR/check.jpg" \
-    "$SPLASH_DIR/nowifi.jpg" \
-    "$SPLASH_DIR/stream.jpg" &
-sleep 1
-
 # wifi config
+fbv "$SPLASH_DIR/check.jpg"
 echo >> "$SPLASH_SOCK"
 if iwgetid -r; then
-    echo >> "$SPLASH_SOCK"
     echo 'Skipping WiFi Connect'
-    sleep 1
 else
-    echo >> "$SPLASH_SOCK"
+    fbv "$SPLASH_DIR/wifi.jpg"
     echo 'Starting WiFi Connect'
     wifi-connect
 fi
 
 # start stream
-echo >> "$SPLASH_SOCK"
+fbv "$SPLASH_DIR/stream.jpg"
 VIDEO=$(youtube-dl -f mp4 -g "$STREAM_URL" 2>&1)
 case "$VIDEO" in
     http*)
