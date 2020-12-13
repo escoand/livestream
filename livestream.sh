03792d673a3d8001ee4fef57a90f537e9a052de7
escoand <escoand@users.noreply.github.com>
2020-12-13 16:53:02 +0100

    Update livestream.sh

--- livestream.sh
+++ livestream.sh
@@ -11,7 +11,7 @@ if iwgetid -r; then
 else
     splash nowifi
     echo 'Starting WiFi Connect'
-    ifconfig wlan0 up
+    ifconfig wlan0 192.168.99.1 netmask 255.255.255.0 up
     hostapd -B /usr/local/etc/hostapd.conf
     dnsmasq -C /usr/local/etc/dnsmasq.conf -d
     sleep 120
