ad7303ace89f2fae5ec9651415b13d5bf571a37c
escoand <escoand@users.noreply.github.com>
2020-12-13 10:04:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 # requirements
 grep -q '^http.*/community$' ||
 sed -n 's|^\(http.*\)/main$|\1/community|p' /etc/apk/repositories >>/etc/apk/repositories
-apk add --no-cache dnsmasq fbida-fbi hostapd imagemagick omxplayer py3-pip ttf-liberation wireless-tools
-pip install youtube-dl
+apk add --no-cache gcc git libjpeg-turbo-dev libpng-dev ttf-liberation
 
 # livestream
 wget -qO /usr/local/bin/livestream https://raw.githubusercontent.com/escoand/livestream/master/livestream.sh
@@ -14,8 +13,17 @@ chmod +x /usr/local/bin/livestream
 wget -qO- https://raw.githubusercontent.com/escoand/livestream/master/splash.sh |
 sh -es
 
+# fbv
+git clone https://github.com/godspeed1989/fbv.git
+cd fbv
+./configure
+make
+cp fbv /usr/local/bin/
+
 # clean
-apk del imagemagick ttf-liberation
+apk del gcc git imagemagick libjpeg-turbo-dev libpng-dev ttf-liberation
+apk add --no-cache dnsmasq hostapd libjpeg-turbo libpng omxplayer py3-pip wireless-tools
+pip install youtube-dl
 
 # persist if installed
 if command -v rc-update lbu >/dev/null; then
@@ -27,6 +35,6 @@ pidfile=/run/$RC_SVCNAME/$RC_SVCNAME.pid
 END
     chmod +x /etc/init.d/livestream
     rc-update add livestream default
-    lbu add /etc/init.d/livestream /usr/local/bin/livestream /usr/local/share/splash/
+    lbu add /etc/init.d/livestream /usr/local/bin/ /usr/local/share/splash/
     lbu commit -d
 fi
