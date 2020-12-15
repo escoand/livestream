51aabd4f356ed1fbeef42ddbf38cb8741719632f
escoand <escoand@users.noreply.github.com>
2020-12-15 15:03:09 +0100

    switch to NM

--- wifi-connect.sh
+++ wifi-connect.sh
@@ -1,24 +1,40 @@
-#!/bin/sh -x
+#!/bin/sh
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
-# disable Network Manager wireless
-gdbus call --system \
-   --dest org.freedesktop.NetworkManager \
-   --object-path /org/freedesktop/NetworkManager \
-   --method org.freedesktop.DBus.Properties.Set \
-    org.freedesktop.NetworkManager WirelessEnabled '<false>'
+nm_dbus() {
+    _PATH="$1"
+    _METHOD="$2"
+    shift 2
+    gdbus call -y -d org.freedesktop.NetworkManager -o "$_PATH" -m "$_METHOD" "$@"
+}
 
 # setup wifi
-rfkill unblock wlan
-ifconfig wlan0 down
-ifconfig wlan0 192.168.99.1 netmask 255.255.255.0 up
-sleep 5
+DEVICE=$(nm_dbus /org/freedesktop/NetworkManager org.freedesktop.NetworkManager.GetDeviceByIpIface wlan0 | grep -o "/org/freedesktop/NetworkManager/Devices/[^']*")
+SSID_AP=Livestream
+UUID_AP=$(cat /proc/sys/kernel/random/uuid)
+CONNECTION_AP=$(nm_dbus /org/freedesktop/NetworkManager org.freedesktop.NetworkManager.AddAndActivateConnection \
+   "{
+     '802-11-wireless': {
+       'mode': <'ap'>,
+       'ssid': <b'$SSID_AP'>
+     },
+     'connection': {
+       'id': <'$SSID_AP'>,
+       'type': <'802-11-wireless'>,
+       'uuid': <'$UUID_AP'>
+     },
+     'ipv4': {
+       'method': <'manual'>,
+       'address-data': <[{
+         'address': <'192.168.99.1'>,
+         'prefix': <@u 24>
+       }]>
+     }
+   }" "$DEVICE" "/" | grep -o "/org/freedesktop/NetworkManager/Settings/[^']*")
+sleep 3
 
 # start services
-# https://github.com/balena-io-playground/access-point-example
-hostapd /usr/local/etc/hostapd.conf &
-PID_HOSTAPD=$!
 dnsmasq -dk -C /usr/local/etc/dnsmasq.conf &
 PID_DNSMASQ=$!
 
@@ -44,7 +60,12 @@ while true; do
         echo '<form action="" method="post">'
         echo '<label>Netzwerk:</label>'
         echo '<select name="network">'
-        iwlist wlan0 scan | sed -n 's|^.*ESSID:"\([^"]*\)"$|<option>\1</option>|p' | sort -u
+        iwlist wlan0 scan |
+        sed -n 's|.* ESSID:"\([^"]*\)".*|<option>\1</option>|p'
+        #sed -n '/^ *Cell /{h;} / ESSID:/{H;g;s|.* Address: \([0-9A-F:]*\).* ESSID:"\([^"]*\)".*|\1 \2|p}' |
+        #sort -t' ' -k2 |
+        #uniq -f1 |
+        #sed 's| |">|; s|^|<option value="|; s|$|</option>|'
         echo '</select>'
         echo '<label>Passwort:</label>'
         echo '<input type="text" name="password" />'
@@ -54,7 +75,7 @@ while true; do
         echo '</html>'
         } |
         nc -l -p 80 |
-	tr -d '\r' |
+        tr -d '\r' |
         sed '1,/^$/d' |
         tr '&' '\n'
     )
@@ -63,47 +84,50 @@ while true; do
 
     # config wifi
     if [ -n "$SSID" ] && [ -n "$KEY" ]; then
+
+        # create new
         UUID=$(cat /proc/sys/kernel/random/uuid)
-        DEVICE=$(gdbus call --system \
-           --dest org.freedesktop.NetworkManager \
-           --object-path /org/freedesktop/NetworkManager \
-           --method org.freedesktop.NetworkManager.GetDeviceByIpIface \
-           wlan0 | cut -d"'" -f2) &&
-        gdbus call --system \
-           --dest org.freedesktop.NetworkManager \
-           --object-path /org/freedesktop/NetworkManager \
-           --method org.freedesktop.NetworkManager.AddAndActivateConnection \
-           "{
-             '802-11-wireless': {
-               'mode': <'infrastructure'>,
-               'security': <'802-11-wireless-security'>,
-               'ssid': <b'$SSID'>
-             },
-             '802-11-wireless-security': {
-               'auth-alg': <'open'>, 
-               'key-mgmt': <'wpa-psk'>,
-               'psk': <'$KEY'>
-             },
-             'connection': {
-               'id': <'$SSID'>,
-               'type': <'802-11-wireless'>,
-               'uuid': <'$UUID'>
-             },
-             'ipv4': {
-               'method': <'auto'>
-             },
-             'ipv6': {
-               'method': <'auto'>
-             }
-           }" "$DEVICE" "/" &&
-        gdbus call --system \
-           --dest org.freedesktop.NetworkManager \
-           --object-path /org/freedesktop/NetworkManager \
-           --method org.freedesktop.DBus.Properties.Set \
-            org.freedesktop.NetworkManager WirelessEnabled '<true>' &&
+        CONNECTION=$(nm_dbus /org/freedesktop/NetworkManager/Settings org.freedesktop.NetworkManager.Settings.AddConnection \
+            "{
+              '802-11-wireless': {
+                'hidden': <true>,
+                'mode': <'infrastructure'>,
+                'security': <'802-11-wireless-security'>,
+                'ssid': <b'$SSID'>
+              },
+              '802-11-wireless-security': {
+                'auth-alg': <'open'>,
+                'key-mgmt': <'wpa-psk'>,
+                'psk': <'$KEY'>
+              },
+              'connection': {
+                'id': <'$SSID'>,
+                'type': <'802-11-wireless'>,
+                'uuid': <'$UUID'>
+              },
+              'ipv4': {
+                'method': <'auto'>
+              },
+              'ipv6': {
+                'method': <'auto'>
+              }
+            }" | grep -o "/org/freedesktop/NetworkManager/Settings/[^']*")
+
+        # activate new
+        ACTIVE=$(nm_dbus /org/freedesktop/NetworkManager org.freedesktop.NetworkManager.ActivateConnection \
+            "$CONNECTION" "$DEVICE" "/" | grep -o "/org/freedesktop/NetworkManager/[^']*")
+        sleep 3
+
+        # successful
+        gdbus introspect -y -d org.freedesktop.NetworkManager -p -o "$ACTIVE" &&
         break
+
+        # next try
+        nm_dbus "$CONNECTION" org.freedesktop.NetworkManager.Settings.Connection.Delete
+        echo "failed to connect to wifi" >&2
     fi
 done
 
 # stop services
-kill $PID_HOSTAPD $PID_DNSMASQ
+kill $PID_DNSMASQ
+nm_dbus "$CONNECTION_AP" org.freedesktop.NetworkManager.Settings.Connection.Delete
