74c272d96634141a7388c1ee965032ae0aba9037
escoand <escoand@users.noreply.github.com>
2021-01-21 00:09:54 +0100

    wait for stream start

--- livestream.sh
+++ livestream.sh
@@ -11,19 +11,29 @@ if ! ping -c1 google.com &>/dev/null; then
     wifi-connect
 fi
 
-# start stream
+# get stream start
 splash stream
 echo "load url $STREAM_URL"
-VIDEO=$(youtube-dl -f mp4 -g "$STREAM_URL" 2>&1)
-echo "result was $VIDEO"
-case "$VIDEO" in
-    http*)
-        omxplayer -o hdmi "$VIDEO" ||
-        splash error
-        ;;
-    *)
-        echo "$VIDEO" >/dev/tty1
-        ;;
-esac 
+NOW=$(date -u +%Y-%m-%dT%H:%M:%S+00:00)
+START=$(
+    youtube-dl -is --dump-pages "$STREAM_URL" |
+    grep -v '^\[' |
+    base64 -d |
+    sed -n 's#.*var ytInitialPlayerResponse *= *\({.*\)#\1#p' |
+    jq -r '.microformat.playerMicroformatRenderer.liveBroadcastDetails.startTimestamp'
+) 2>/dev/null
+
+# wait for start
+if [ -n "$START" ] && [ "$START" != null ] && [ "$START" < "$NOW" ]; then
+    splash scheduled
+    TS_NOW=$(date +%s)
+    TS_START=$(date +%s -d "$START")
+    sleep $(($TS_START - $TS_NOW + 10)
+fi
+
+# play stream
+youtube-dl -f mp4 -g "$STREAM_URL" |
+omxplayer -o hdmi "$VIDEO" ||
+splash error
 
 sleep infinity
