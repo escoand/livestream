0569fc2e4edc2eb534a651347ea9be22c3c3a4ac
escoand <escoand@users.noreply.github.com>
2021-01-21 22:31:25 +0100

    show video thumbnail

--- livestream.sh
+++ livestream.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 splash() {
-    fbv -cefir "/usr/local/share/splash/$1.jpg" </dev/null
+    if [ -z "${1##*/*}" ]; then
+        fbv -cefir "$1" </dev/null
+    else
+        fbv -cefir "/usr/local/share/splash/$1.jpg" </dev/null
+    fi
 }
 
 # wifi config
@@ -15,18 +19,23 @@ fi
 splash stream
 echo "load url $STREAM_URL"
 NOW=$(date -u +%s)
-START=$(
+JSON=$(
     youtube-dl -is --dump-pages "$STREAM_URL" |
     grep -v '^\[' |
     base64 -d |
     sed -n 's#.*var ytInitialPlayerResponse *= *{#{#p' |
-    jq -r '.microformat.playerMicroformatRenderer.liveBroadcastDetails.startTimestamp[:19] + "Z" | fromdate?'
+    jq -c 2>/dev/null
 )
+START=$(echo "$JSON" | jq -r '.microformat.playerMicroformatRenderer.liveBroadcastDetails.startTimestamp[:19] + "Z" | fromdate?')
+THUMB=$(echo "$JSON" | jq -r '.videoDetails.thumbnail.thumbnails[-1].url')
 
 # wait for start
 if [ -n "$START" ] && [ "$START" != null ] && [ "$START" -gt "$NOW" ]; then
     START_STR=$(date +"%d.%m.%Y %H:%M" -d @"$START")
-    splash scheduled "$START_STR"
+    TMP=$(mktemp)
+    curl -sSo "$TMP" "$THUMB" &&
+    splash "$TMP" ||
+    splash scheduled
     sleep $((START - NOW + 10))
 fi
 
